9dc90f9a4d7a40ac8e91d636247cfdec4e74ef3b
Raj106292 <rajdas.106292@gmail.com>
2023-03-24 11:16:46 +0600

    simple counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [countValue, setCountValue] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='app'>
+        <div className='counterValue'>
+          {countValue}
+        </div>
+        <div className='button-group'>
+          <button className='decrementBtn' onClick={() => setCountValue(countValue - 1)}><span>Decrement</span></button>
+          <button className='incrementBtn' onClick={() => setCountValue(countValue + 1)}><span>Increment</span></button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/* 
+  => we need to explore useReducer() hook before Redux
+  => we pass the initial state value in the useState() hook
+  => useState() hook returns an array which has 2 items.
+    -> first one is the state
+    -> second one is the set function which change the state value
+  => React automatically handle re-rendering part
+  => a state which is declared inside a component called local state
+*/
